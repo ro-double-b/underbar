1689659649983d09ae4eb43062f82885b9183c8d
Robb Marsee <robb.marsee@gamil.com>
2016-04-20 21:58:05 -0700

    "completed: reject"

--- src/underbar.js
+++ src/underbar.js
@@ -90,8 +90,18 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+
+    var result = []
+    _.each(collection, function(item) {
+      if(!test(item)) {
+        result.push(item)
+      }
+    })
+      return result
+
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+
   };
 
   // Produce a duplicate-free version of the array.
